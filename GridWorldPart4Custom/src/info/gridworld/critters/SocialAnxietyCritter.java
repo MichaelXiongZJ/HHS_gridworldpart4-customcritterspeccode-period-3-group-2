23f72806887ae642d6cf397b36298ad0945dba8b
Zejun Xiong <69989842+MichaelThePanda@users.noreply.github.com>
2021-02-17 15:15:36 -0800

    getMoveLocation() update

--- GridWorldPart4Custom/src/info/gridworld/critters/SocialAnxietyCritter.java
+++ GridWorldPart4Custom/src/info/gridworld/critters/SocialAnxietyCritter.java
@@ -1,5 +1,7 @@
 package info.gridworld.critters;
 
+import java.util.ArrayList;
+
 import info.gridworld.actor.*;
 import info.gridworld.grid.*;
 
@@ -9,8 +11,6 @@ import info.gridworld.grid.*;
  */
 public class SocialAnxietyCritter extends Critter
 {
-
-	
 	
 	public void makeMove(Location loc) {
 		int newDirection = getLocation().getDirectionToward(loc);
@@ -22,5 +22,32 @@ public class SocialAnxietyCritter extends Critter
         else {
             moveTo(loc); 
         }
+	}
+	
+	public ArrayList<Location> getMoveLocations() {
+		//Find closest actor
+		ArrayList<Actor> listOfActors = getActors();
+		int x = getLocation().getCol();
+		int y = getLocation().getRow();
+		int actorIndex = 0;
+		double[] listOfDistances = new double [getActors().size()];
+		double shortestDistance = Double.MAX_VALUE;
+		for (int a=0; a<listOfActors.size(); a++) {
+			double distance = Math.sqrt(Math.pow(listOfActors.get(a).getLocation().getRow()-y, 2) + Math.pow(listOfActors.get(a).getLocation().getCol()-x, 2));
+			listOfDistances[a] = distance;
+		}
+		for (int a=0; a<listOfDistances.length; a++) {
+			if (shortestDistance < listOfDistances[a]) {
+				shortestDistance = listOfDistances[a];
+				actorIndex = a;
+			}
+		}
+		//Find the location to go
+		int xDiff = x - listOfActors.get(actorIndex).getLocation().getCol();
+		int yDiff = y - listOfActors.get(actorIndex).getLocation().getRow();
+		if( (xDiff==0&&yDiff<0) || ())
+		//Check if location valid
+		
+		return null;
 	}
 }
